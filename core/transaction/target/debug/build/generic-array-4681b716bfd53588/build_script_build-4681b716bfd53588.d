1d1ebd2a17d0cd87c5da9c67bdc4361c9a8643d7
Sunaked <syrgulev-aa@mail.ru>
2022-05-18 10:15:37 +0300

    Made Serialize and Deserialize function for Transaction struct

---
+++ core/transaction/target/debug/build/generic-array-4681b716bfd53588/build_script_build-4681b716bfd53588.d
@@ -0,0 +1,5 @@
+/home/yourgod/programming/Rust/Blockchain-Project/Unit-unit-rust-reborn/core/transaction/target/debug/build/generic-array-4681b716bfd53588/build_script_build-4681b716bfd53588: /home/yourgod/.cargo/registry/src/github.com-1ecc6299db9ec823/generic-array-0.14.5/build.rs
+
+/home/yourgod/programming/Rust/Blockchain-Project/Unit-unit-rust-reborn/core/transaction/target/debug/build/generic-array-4681b716bfd53588/build_script_build-4681b716bfd53588.d: /home/yourgod/.cargo/registry/src/github.com-1ecc6299db9ec823/generic-array-0.14.5/build.rs
+
+/home/yourgod/.cargo/registry/src/github.com-1ecc6299db9ec823/generic-array-0.14.5/build.rs:
