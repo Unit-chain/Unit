1a47a4b5b8f559464591eaa3920b7c241c7bd3c5
Sunaked <syrgulev-aa@mail.ru>
2022-05-20 18:08:26 +0300

    Transaction to binary; two step hash; test transaction_to_binary()
    Added "binary" method to transaction struct
    Updated "hash" method to transaction struct: it hashes now two times, instead of one time
    
    Added "transaction_to_binary" test

--- core/transaction/src/lib.rs
+++ core/transaction/src/lib.rs
@@ -53,7 +53,13 @@ mod tests {
         let tx = transaction::new_forgerie_transaction(232u128);
         use super::*;
         println!("{}", tx.serialize());
-        assert_eq!(tx.hash(), "44e9695058c7d88a728d79d56b9d3c14c448eeb8bda35ebe3d1d6d4d26297b9b275c8a6da9aa7b19135d1c545ddc6d2b")
+        assert_eq!(tx.hash(), "65e4037cba825c093499b6df3f26cf8bd8255e594836aec1f3355a1a656b014e25accdf0579d4682eed6108eac08ecdc")
+    }
+
+    #[test]
+    fn transaction_to_binary() {
+        let tx = transaction::new_forgerie_transaction(232u128);
+        assert_eq!(tx.binary(), "00110100 00110100 01100101 00111001 00110110 00111001 00110101 00110000 00110101 00111000 01100011 00110111 01100100 00111000 00111000 01100001 00110111 00110010 00111000 01100100 00110111 00111001 01100100 00110101 00110110 01100010 00111001 01100100 00110011 01100011 00110001 00110100 01100011 00110100 00110100 00111000 01100101 01100101 01100010 00111000 01100010 01100100 01100001 00110011 00110101 01100101 01100010 01100101 00110011 01100100 00110001 01100100 00110110 01100100 00110100 01100100 00110010 00110110 00110010 00111001 00110111 01100010 00111001 01100010 00110010 00110111 00110101 01100011 00111000 01100001 00110110 01100100 01100001 00111001 01100001 01100001 00110111 01100010 00110001 00111001 00110001 00110011 00110101 01100100 00110001 01100011 00110101 00110100 00110101 01100100 01100100 01100011 00110110 01100100 00110010 01100010 ");
     }
 }
 
@@ -202,9 +208,19 @@ pub mod transaction {
             // write input message
             hasher.update(format!("{}", self.serialize()));
             // read hash digest
+            let one_hash = hasher.finalize();
+
+            let mut hasher = Sha3_384::new();
+            hasher.update(format!("{}", hex::encode(one_hash)));
             format!("{}", hex::encode(hasher.finalize()))
         }
-
+        pub fn binary(&self) -> String {
+            let mut binary = "".to_string();
+            for character in self.hash().clone().into_bytes() {
+                binary += &format!("0{:b} ", character);
+            }
+            format!("{}", binary)
+        }
         // fn sign(&self, )
     }
     pub fn deserialize_transaction(transaction: String) -> Transaction {
